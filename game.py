c2c6dd573ff24ecb6c8d926a2ee7bc31a33a4959
Vlad <iravysochan88@gmail.com>
2023-10-29 14:31:25 +0100

    added images, pipes and finish

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame 
+from random import randint
 
 
 WIDTH = 1300
@@ -13,7 +14,8 @@ clock = pygame.time.Clock()
 
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, filename, coords, size, speed):
-        self.image = pygame.Surface(size=size, masks=(255,255,255))
+        self.image = pygame.transform.scale(pygame.image.load(filename), size)
+        self.original = pygame.transform.scale(pygame.image.load(filename), size)
         self.rect = self.image.get_rect()
         self.rect.center = coords
         self.speed = speed
@@ -28,8 +30,26 @@ class Bird(GameSprite):
         if keys[pygame.K_SPACE]:
             self.rect.y -= self.speed*2
 
+class Tube(GameSprite):
+    def update_up(self):
+        self.rect.x -= self.speed
+        if self.rect.x <= 0:
+            self.rect.x = WIDTH
+            self.rect = pygame.Rect(self.rect.x,self.rect.y, self.rect.width, randint(50,HEIGHT/2-50))
+            self.image = pygame.Surface(size=(self.rect.width, self.rect.height))
+            self.image = pygame.transform.scale(self.original, (self.rect.width,self.rect.height))
 
-bird = Bird(None, (50, HEIGHT/2), (50,50), 3)
+    def update_down(self):
+        self.rect.x -= self.speed
+        if self.rect.x <= 0:
+            self.rect.x = WIDTH
+            self.rect = pygame.Rect(self.rect.x, randint(HEIGHT/2, HEIGHT-100), self.rect.width, self.rect.height)
+            self.image = pygame.Surface(size=(self.rect.width, self.rect.height))
+            self.image = pygame.transform.scale(self.original, (self.rect.width,self.rect.height))
+
+bird = Bird("bird.png", (50, HEIGHT/2), (50,50), 4)
+up_tube = Tube("tube_up.png", (WIDTH, 100), (75, 200), 4)
+down_tube = Tube("tube_down.png", (WIDTH, HEIGHT+100), (75, 600), 4)
 
 game = True
 finish = False
@@ -37,9 +57,22 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-    window.fill(background_color)
-    bird.reset()
-    bird.update()
+    if not finish:
+        window.fill(background_color)
+        bird.update()
+        bird.reset()
+        up_tube.update_up()
+        up_tube.reset()
+        down_tube.update_down()
+        down_tube.reset()
+
+        if pygame.sprite.collide_rect(bird,up_tube) or pygame.sprite.collide_rect(bird,down_tube):
+            finish = True
+            pygame.font.init()
+            font1 = pygame.font.Font(None,60)
+            text = font1.render("Ти програв", True, (255,0,0))
+            window.blit(text, (WIDTH/2-100, HEIGHT/2))
+
 
     pygame.display.update()
     clock.tick(60)
