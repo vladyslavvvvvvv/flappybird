72f35fddd2a657e1ba610a9ff228547630fd249a
Vlad <iravysochan88@gmail.com>
2023-11-05 13:42:49 +0100

    finished game

--- game.py
+++ game.py
@@ -9,6 +9,15 @@ SIZE = (WIDTH,HEIGHT)
 
 window = pygame.display.set_mode(SIZE)
 background_color = (60, 160, 65)
+background_image = pygame.transform.scale(pygame.image.load("background.png"), SIZE)
+
+score = 0
+
+pygame.font.init()
+font2 = pygame.font.Font(None, 40)
+
+
+
 window.fill(background_color)
 clock = pygame.time.Clock()
 
@@ -34,18 +43,24 @@ class Tube(GameSprite):
     def update_up(self):
         self.rect.x -= self.speed
         if self.rect.x <= 0:
-            self.rect.x = WIDTH
+            self.rect.x = WIDTH + randint(0,100)
             self.rect = pygame.Rect(self.rect.x,self.rect.y, self.rect.width, randint(50,HEIGHT/2-50))
             self.image = pygame.Surface(size=(self.rect.width, self.rect.height))
             self.image = pygame.transform.scale(self.original, (self.rect.width,self.rect.height))
+            global score
+            score += 1
+            self.speed = randint(3,10)
 
     def update_down(self):
         self.rect.x -= self.speed
         if self.rect.x <= 0:
-            self.rect.x = WIDTH
+            self.rect.x = WIDTH + randint(0,100)
             self.rect = pygame.Rect(self.rect.x, randint(HEIGHT/2, HEIGHT-100), self.rect.width, self.rect.height)
             self.image = pygame.Surface(size=(self.rect.width, self.rect.height))
             self.image = pygame.transform.scale(self.original, (self.rect.width,self.rect.height))
+            global score
+            score += 1
+            self.speed = randint(3,10)
 
 bird = Bird("bird.png", (50, HEIGHT/2), (50,50), 4)
 up_tube = Tube("tube_up.png", (WIDTH, 100), (75, 200), 4)
@@ -57,8 +72,9 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+
     if not finish:
-        window.fill(background_color)
+        window.blit(background_image, (0,0))
         bird.update()
         bird.reset()
         up_tube.update_up()
@@ -73,9 +89,8 @@ while game:
             text = font1.render("Ти програв", True, (255,0,0))
             window.blit(text, (WIDTH/2-100, HEIGHT/2))
 
+        score_text = font2.render(str(score), True, (255,255,255))
+        window.blit(score_text, (0,0))
 
     pygame.display.update()
     clock.tick(60)
-
-
-
